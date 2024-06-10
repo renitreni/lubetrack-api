b73e09b933f332891e3e198e5bfcf90ca5775e64
Renier Trenuela <renier.trenuela@gmail.com>
2024-06-10 09:46:01 +0800

    wip

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,13 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function ok($json = [], $code = 200)
+    {
+        return response()->json($json, $code);
+    }
+
+    public function error($json = [], $code = 500)
+    {
+        return response()->json($json, $code);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
-
-    return ['token' => $token->plainTextToken];
-});
+Route::post('/tokens/create', [LoginController::class, 'authenticate']);
